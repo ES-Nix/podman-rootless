3dd699c24d6d23c4a48e1f9519255fbe92e5333d
pedroregispoar <pedroalencarregis@hotmail.com>
2021-02-06 20:49:15 +0000

    MVP of compleInstallPodman only echoing works

--- flake.nix
+++ flake.nix
@@ -6,33 +6,49 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+         pkgsAllowUnfree = import nixpkgs {
+           system = "x86_64-linux";
+           config = { allowUnfree = true; };
+         };
 
-        pkgsAllowUnfree = import nixpkgs {
-          system = "x86_64-linux";
-          config = { allowUnfree = true; };
-        };
-
+        myScript =  pkgsAllowUnfree.writeShellScriptBin "compleInstallPodman" ''
+          #!${nixpkgs.legacyPackages.${system}.stdenv.shell}
+          echo 'The wrapper!'
+       '';
       in
       {
+
+        myScript =  pkgsAllowUnfree.writeShellScriptBin "compleInstallPodman" ''
+          #!${nixpkgs.legacyPackages.${system}.stdenv.shell}
+          echo 'The wrapper!'
+       '';
+
         # For FREE packages use:
-        packages.podman = import ./podman.nix {
-            pkgs = nixpkgs.legacyPackages.${system};
-        };
+        #packages.podman = import ./podman.nix {
+        #    pkgs = nixpkgs.legacyPackages.${system};
+        #};
 
         #packages.podman = import ./podman.nix {
         #  pkgs = pkgsAllowUnfree;
         #};
 
-        defaultPackage = self.packages.${system}.podman;
+        #defaultPackage = self.packages.${system}.podman;
 
         devShell = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
-                          conmon
-                          podman
-                          runc
-                          shadow
-                          slirp4netns
-                     ];
+                                     
+                                         conmon
+                                         podman
+                                         runc
+                                         shadow
+                                         slirp4netns                          
+                                         myScript
+                          
+                          ];
+        shellHook = ''
+           echo "The hook"
+         '';
+
         };
 
       });
