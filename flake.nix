24561220a003e457c00f3150079eb99bfcd78374
Pedro Regis <pedroalencarregis@hotmail.com>
2021-02-07 16:48:29 -0300

    Format and better names

--- flake.nix
+++ flake.nix
@@ -6,13 +6,12 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-         pkgsAllowUnfree = import nixpkgs {
-           system = "x86_64-linux";
-           config = { allowUnfree = true; };
-         };
+        pkgsAllowUnfree = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
 
-
-        myScript =  pkgsAllowUnfree.writeShellScriptBin "extraPodman" ''
+        podmanCapabilities = pkgsAllowUnfree.writeShellScriptBin "podman-capabilities" ''
           #!${nixpkgs.legacyPackages.${system}.stdenv.shell}
           
           # TODO: add a conditional here to run this mesage only when 
@@ -30,69 +29,63 @@
 
           sudo chmod -s "$NEWUIDMAP"
           sudo chmod -s "$NEWGIDMAP"
+        '';
 
-       '';
+        # Provides a script that copies required files to ~/
+        podmanSetupScript =
+          let
+            registriesConf = pkgsAllowUnfree.writeText "registries.conf" ''
+              [registries.search]
+              registries = ['docker.io']
+              [registries.block]
+              registries = []
+            '';
+          in
+          pkgsAllowUnfree.writeShellScriptBin "podman-setup" ''
+            #!${pkgsAllowUnfree.runtimeShell}
+            # Dont overwrite customised configuration
+            if ! test -f ~/.config/containers/policy.json; then
+              install -Dm555 ${pkgsAllowUnfree.skopeo.src}/default-policy.json ~/.config/containers/policy.json
+            fi
+            if ! test -f ~/.config/containers/registries.conf; then
+              install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
+            fi
+          '';
 
+        # Provides a fake "docker" binary mapping to podman
+        dockerCompat = pkgsAllowUnfree.runCommandNoCC "docker-podman-compat" { } ''
+          mkdir --parent $out/bin
+          ln --symbolic ${pkgsAllowUnfree.podman}/bin/podman $out/bin/docker
+        '';
 
-  # Provides a script that copies required files to ~/
-  podmanSetupScript = let
-    registriesConf = pkgsAllowUnfree.writeText "registries.conf" ''
-      [registries.search]
-      registries = ['docker.io']
-      [registries.block]
-      registries = []
-    '';
-  in pkgsAllowUnfree.writeShellScriptBin "podman-setup" ''
-    #!${pkgsAllowUnfree.runtimeShell}
-    # Dont overwrite customised configuration
-    if ! test -f ~/.config/containers/policy.json; then
-      install -Dm555 ${pkgsAllowUnfree.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-    fi
-    if ! test -f ~/.config/containers/registries.conf; then
-      install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-    fi
-  '';
-
-
-  # Provides a fake "docker" binary mapping to podman
-  dockerCompat = pkgsAllowUnfree.runCommandNoCC "docker-podman-compat" {} ''
-    mkdir -p $out/bin
-    ln -s ${pkgsAllowUnfree.podman}/bin/podman $out/bin/docker
-  '';
-
-  cleanPodmanSetup = pkgsAllowUnfree.writeShellScriptBin "clear-podman-setup" ''
-    #!${pkgsAllowUnfree.runtimeShell}
+        podmanClearConfigFiles = pkgsAllowUnfree.writeShellScriptBin "podman-clear-config-files" ''
+          #!${pkgsAllowUnfree.runtimeShell}
   
-     rm --force --verbose ~/.config/containers/policy.json
-     rm --force --verbose ~/.config/containers/registries.conf
-   
-  '';
+           rm --force --verbose ~/.config/containers/policy.json
+           rm --force --verbose ~/.config/containers/registries.conf
+        '';
 
-  clearPodmanCreatedContainersAndMore = pkgsAllowUnfree.writeShellScriptBin "podman-clear" ''
-    #!${pkgsAllowUnfree.runtimeShell}
+        podmanClearItsData = pkgsAllowUnfree.writeShellScriptBin "podman-clear-its-data" ''
+          #!${pkgsAllowUnfree.runtimeShell}
     
-    # TODO: it need tests!
-    podman ps --all --quiet | xargs --no-run-if-empty podman stop \
-    && podman ps --all --quiet | xargs --no-run-if-empty podman rm --force\
-    && podman images --quiet | xargs --no-run-if-empty podman rmi --force \
-    && podman container prune --force \
-    && podman images --quiet | podman image prune --force \
-    && podman network ls --quiet | xargs --no-run-if-empty podman network rm \
-    && podman volume ls --quiet | xargs --no-run-if-empty podman volume prune
-    '';
+          # TODO: it need tests!
+          podman ps --all --quiet | xargs --no-run-if-empty podman stop \
+          && podman ps --all --quiet | xargs --no-run-if-empty podman rm --force\
+          && podman images --quiet | xargs --no-run-if-empty podman rmi --force \
+          && podman container prune --force \
+          && podman images --quiet | podman image prune --force \
+          && podman network ls --quiet | xargs --no-run-if-empty podman network rm \
+          && podman volume ls --quiet | xargs --no-run-if-empty podman volume prune
+        '';
 
+        # TODO: it does not work
+        testsPodmanInstall = pkgsAllowUnfree.writeShellScriptBin "tests-podman-install" ''
+          #!${pkgsAllowUnfree.runtimeShell}
 
-  # TODO: it does not work
-  testsPodmanInstall = pkgsAllowUnfree.writeShellScriptBin "tests-podman-install" ''
-    #!${pkgsAllowUnfree.runtimeShell}
-      
-
-     touch $out
-     cp ${./tests.sh} $out/${./tests.sh}
-     .$out/tests.sh
-
-  '';
-
+           touch $out
+           cp ${./tests.sh} $out/${./tests.sh}
+           .$out/tests.sh
+        '';
 
       in
       {
@@ -110,23 +103,23 @@
 
         devShell = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
-                                     
-                                         conmon
-                                         podman
-                                         runc
-                                         shadow
-                                         slirp4netns                          
-                                         myScript
-                                         podmanSetupScript
-                                         cleanPodmanSetup
-                                         testsPodmanInstall
-                                         clearPodmanCreatedContainersAndMore
-                          ];
-        shellHook = ''
-           #echo "Entering the nix devShell"
-           podman-setup
-           extraPodman
-         '';
+            conmon
+            podman
+            runc
+            shadow
+            slirp4netns
+            podmanCapabilities
+            podmanSetupScript # config files in here
+            podmanClearConfigFiles
+            #testsPodmanInstall
+            podmanClearItsData
+            dockerCompat
+          ];
+          shellHook = ''
+            #echo "Entering the nix devShell"
+            podman-setup
+            podman-capabilities
+          '';
 
         };
 
