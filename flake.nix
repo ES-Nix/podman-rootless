170f002d76070b1d281cf7e6868076bcfb1fea07
Pedro Regis <pedroalencarregis@hotmail.com>
2021-02-08 00:43:01 -0300

    Saves to test

--- flake.nix
+++ flake.nix
@@ -11,82 +11,6 @@
           config = { allowUnfree = true; };
         };
 
-        podmanCapabilities = pkgsAllowUnfree.writeShellScriptBin "podman-capabilities" ''
-          #!${nixpkgs.legacyPackages.${system}.stdenv.shell}
-          
-          # TODO: add a conditional here to run this mesage only when 
-          # needs a sudo call, i mean, only the first time problably.
-          # No call for sudo is neede after de first time (in most cases)
-          # We should check for the actual capabilitie and if they are 
-          # the ones that podman needs skip the sudo calls.
-
-          #echo 'Fixing capabilities. It requires sudo, sorry!'
-          NEWUIDMAP=$(readlink --canonicalize $(which newuidmap))
-          NEWGIDMAP=$(readlink --canonicalize $(which newgidmap))
-
-          sudo setcap cap_setuid+ep "$NEWUIDMAP"
-          sudo setcap cap_setgid+ep "$NEWGIDMAP"
-
-          sudo chmod -s "$NEWUIDMAP"
-          sudo chmod -s "$NEWGIDMAP"
-        '';
-
-        # Provides a script that copies required files to ~/
-        podmanSetupScript =
-          let
-            registriesConf = pkgsAllowUnfree.writeText "registries.conf" ''
-              [registries.search]
-              registries = ['docker.io']
-              [registries.block]
-              registries = []
-            '';
-          in
-          pkgsAllowUnfree.writeShellScriptBin "podman-setup" ''
-            #!${pkgsAllowUnfree.runtimeShell}
-            # Dont overwrite customised configuration
-            if ! test -f ~/.config/containers/policy.json; then
-              install -Dm555 ${pkgsAllowUnfree.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-            fi
-            if ! test -f ~/.config/containers/registries.conf; then
-              install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-            fi
-          '';
-
-        # Provides a fake "docker" binary mapping to podman
-        dockerCompat = pkgsAllowUnfree.runCommandNoCC "docker-podman-compat" { } ''
-          mkdir --parent $out/bin
-          ln --symbolic ${pkgsAllowUnfree.podman}/bin/podman $out/bin/docker
-        '';
-
-        podmanClearConfigFiles = pkgsAllowUnfree.writeShellScriptBin "podman-clear-config-files" ''
-          #!${pkgsAllowUnfree.runtimeShell}
-  
-           rm --force --verbose ~/.config/containers/policy.json
-           rm --force --verbose ~/.config/containers/registries.conf
-        '';
-
-        podmanClearItsData = pkgsAllowUnfree.writeShellScriptBin "podman-clear-its-data" ''
-          #!${pkgsAllowUnfree.runtimeShell}
-    
-          # TODO: it need tests!
-          podman ps --all --quiet | xargs --no-run-if-empty podman stop \
-          && podman ps --all --quiet | xargs --no-run-if-empty podman rm --force\
-          && podman images --quiet | xargs --no-run-if-empty podman rmi --force \
-          && podman container prune --force \
-          && podman images --quiet | podman image prune --force \
-          && podman network ls --quiet | xargs --no-run-if-empty podman network rm \
-          && podman volume ls --quiet | xargs --no-run-if-empty podman volume prune
-        '';
-
-        # TODO: it does not work
-        testsPodmanInstall = pkgsAllowUnfree.writeShellScriptBin "tests-podman-install" ''
-          #!${pkgsAllowUnfree.runtimeShell}
-
-           touch $out
-           cp ${./tests.sh} $out/${./tests.sh}
-           .$out/tests.sh
-        '';
-
       in
       {
         # For FREE packages use:
@@ -104,31 +28,15 @@
 
         devShell = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
-            conmon
-            podman
-            runc
-            shadow
-            slirp4netns
-            podmanCapabilities
-            podmanSetupScript # config files in here
-            podmanClearConfigFiles
-            #testsPodmanInstall # not working
-            podmanClearItsData
-            dockerCompat
-            #mypodman.defaultPackage.${system}
             self.defaultPackage.${system}
             self.packages.${system}.mypodman
           ];
           shellHook = ''
             #echo "Entering the nix devShell"
-            podman-setup
+            podman-setup-script
             podman-capabilities
-            test_script
-            compleInstallPodman
+            completInstallPodman
           '';
-
         };
-
       });
-
 }
--- podman.nix
+++ podman.nix
@@ -1,10 +1,77 @@
-{ pkgs ? import <nixpkgs> { } }:
+{pkgs ? import <nixpkgs> { } }:
 let
 
-  myScript = pkgs.writeShellScriptBin "compleInstallPodman" ''
+  myScript = pkgs.writeShellScriptBin "completInstallPodman" ''
     #!${pkgs.stdenv.shell}
     echo 'The wrapper!'
   '';
+
+        podmanCapabilities = pkgs.writeShellScriptBin "podman-capabilities" ''
+          #!${pkgs.runtimeShell}
+
+          # TODO: add a conditional here to run this message only when
+          # needs a sudo call, i mean, only the first time problably.
+          # No call for sudo is neede after de first time (in most cases)
+          # We should check for the actual capabilitie and if they are
+          # the ones that podman needs skip the sudo calls.
+
+          #echo 'Fixing capabilities. It requires sudo, sorry!'
+          NEWUIDMAP=$(readlink --canonicalize $(which newuidmap))
+          NEWGIDMAP=$(readlink --canonicalize $(which newgidmap))
+
+          sudo setcap cap_setuid+ep "$NEWUIDMAP"
+          sudo setcap cap_setgid+ep "$NEWGIDMAP"
+
+          sudo chmod -s "$NEWUIDMAP"
+          sudo chmod -s "$NEWGIDMAP"
+        '';
+
+        # Provides a script that copies required files to ~/
+        podmanSetupScript =
+          let
+            registriesConf = pkgs.writeText "registries.conf" ''
+              [registries.search]
+              registries = ['docker.io']
+              [registries.block]
+              registries = []
+            '';
+          in
+          pkgs.writeShellScriptBin "podman-setup-script" ''
+            #!${pkgs.runtimeShell}
+            # Dont overwrite customised configuration
+            if ! test -f ~/.config/containers/policy.json; then
+              install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
+            fi
+            if ! test -f ~/.config/containers/registries.conf; then
+              install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
+            fi
+          '';
+
+        # Provides a fake "docker" binary mapping to podman
+        dockerPodmanCompat = pkgs.runCommandNoCC "docker-podman-compat" { } ''
+          mkdir --parent $out/bin
+          ln --symbolic ${pkgs.podman}/bin/podman $out/bin/docker
+        '';
+
+        podmanClearConfigFiles = pkgs.writeShellScriptBin "podman-clear-config-files" ''
+          #!${pkgs.runtimeShell}
+
+           rm --force --verbose ~/.config/containers/policy.json
+           rm --force --verbose ~/.config/containers/registries.conf
+        '';
+
+        podmanClearItsData = pkgs.writeShellScriptBin "podman-clear-its-data" ''
+          #!${pkgs.runtimeShell}
+
+          # TODO: it need tests!
+          podman ps --all --quiet | xargs --no-run-if-empty podman stop \
+          && podman ps --all --quiet | xargs --no-run-if-empty podman rm --force\
+          && podman images --quiet | xargs --no-run-if-empty podman rmi --force \
+          && podman container prune --force \
+          && podman images --quiet | podman image prune --force \
+          && podman network ls --quiet | xargs --no-run-if-empty podman network rm \
+          && podman volume ls --quiet | xargs --no-run-if-empty podman volume prune
+        '';
 
 in
 pkgs.stdenv.mkDerivation {
@@ -16,9 +83,11 @@ pkgs.stdenv.mkDerivation {
     runc
     shadow
     slirp4netns
+    dockerPodmanCompat
   ];
   #src = builtins.filterSource (path: type: false) ./.;
   #unpackPhase = "true";
+  #src = self;
 #  buildPhase = ''
 #    mkdir --parent $out
 #    cp -arv ${pkgs.conmon}/bin/conmon $out/conmon
@@ -31,6 +100,8 @@ pkgs.stdenv.mkDerivation {
 #
 #  '';
 
+  #buildPhase = dockerPodmanCompat;
+
   installPhase = ''
     mkdir --parent $out/bin;
     install -t $out/bin ${pkgs.podman}/bin/podman
@@ -39,9 +110,15 @@ pkgs.stdenv.mkDerivation {
     install -t $out/bin ${pkgs.shadow}/bin/newuidmap
     install -t $out/bin ${pkgs.shadow}/bin/newgidmap
     install -t $out/bin ${pkgs.slirp4netns}/bin/slirp4netns
-    install -t $out/bin ${myScript}/bin/compleInstallPodman
+
+    install -t $out/bin ${myScript}/bin/completInstallPodman
+    install -t $out/bin ${podmanCapabilities}/bin/podman-capabilities
+    install -t $out/bin ${podmanSetupScript}/bin/podman-setup-script
+    install -t $out/bin ${podmanClearConfigFiles}/bin/podman-clear-config-files
+    install -t $out/bin ${podmanClearItsData}/bin/podman-clear-its-data
+
     '';
 
-  phases = [ "installPhase" "fixupPhase" ];
+  phases = [ "buildPhase" "installPhase" "fixupPhase" ];
 
 }
