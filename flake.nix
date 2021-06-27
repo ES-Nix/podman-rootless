d31bd3e106171a9442bfb560e186a5eb8011c7f7
Pedro Regis <pedroalencarregis@hotmail.com>
2021-06-27 05:55:32 +0000

    Saves to test

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "This is a nix flake podman rootless wrapper";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+
+        # Provides a script that copies required files to ~/
+        podmanSetupScript =
+          let
+            registriesConf = pkgs.writeText "registries.conf" ''
+              [registries.search]
+              registries = ['docker.io']
+              [registries.block]
+              registries = []
+            '';
+          in
+          pkgs.writeShellScriptBin "podman-setup-script" ''
+            if [ "$(id --user)" = "0" ]; then
+              # Dont overwrite customised configuration
+              if ! test -f /etc/containers/policy.json; then
+                install -Dm555 ${pkgs.skopeo.src}/default-policy.json /etc/containers/policy.json
+              fi
+
+              if ! test -f /etc/containers/registries.conf; then
+                install -Dm555 ${registriesConf} /etc/containers/registries.conf
+              fi
+
+            else
+              # Dont overwrite customised configuration
+              if ! test -f ~/.config/containers/policy.json; then
+                install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
+              fi
+
+              if ! test -f ~/.config/containers/registries.conf; then
+                install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
+              fi
+            fi
+          '';
+      in
+      {
+        
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            podman
+            podmanSetupScript
+         ];
+
+          shellHook = ''
+            # TODO: document this
+            export TMPDIR=/tmp
+
+            podman-setup-script	   
+          '';
+        };
+      });
+}
+
